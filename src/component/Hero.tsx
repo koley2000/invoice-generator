7f1471a15aaea248e19ef4472bb2ed2e6799e1d4
koley2000 <koleyz1965@gmail.com>
2024-08-22 20:36:44 +0530

    Second Commit

--- src/component/Hero.tsx
+++ src/component/Hero.tsx
@@ -7,7 +7,7 @@ function Hero() {
             <div className="relative isolate px-6 pt-6 lg:px-9">
                 <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
                     <div className="text-center">
-                        <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl ">Online Invoice Generator</h2>
+                        <h2 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Online Invoice Generator</h2>
                         <p className="mt-6 text-lg leading-8 text-gray-600 ">Welcome! Invoice Generator lets you 
                         instantly make invoices with our attractive invoice template straight from your web browser. The invoices you make can be downloaded 
                         as PDF.</p>
